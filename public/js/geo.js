1d8c5a5dc980db2de327cc3614d7c80feacb1ff1
Gilberth Segura <gilberthsegura@outlook.com>
2020-12-13 15:34:39 -0600

    Despliegue del mapa

---
+++ public/js/geo.js
@@ -0,0 +1,7 @@
+var coord = [9.838701,-83.946894];
+var map = L.map('map-template').setView([9.838701,-83.946894], 15);
+
+L.tileLayer('https://a.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+var marker = L.marker(coord).bindPopup('Evisto por aquí');
+map.addLayer(marker);
