c4fd10bf6c7f9a467dab7d8fc2fa676b86c57d0c
Daar375 <Daar375@gmail.com>
2020-12-16 18:52:19 -0600

    Update geo.js

--- public/js/geo.js
+++ public/js/geo.js
@@ -1,26 +1,33 @@
 var loc = db.ref('Localizaciones');
-var coord = [0,0];
 
-loc.on('value', function(snapshot) {
+var p1 = new Promise((resolve, reject) => {loc.on('value', function(snapshot) {
     snapshot.forEach(function(childSnapshot) {
       var childData = childSnapshot.val();
       console.log(childData);
       if("117140802"==childData.ced){
         console.log([childData.latitude, childData.longitude]) 
-            coord = [childData.latitude, childData.longitude];
-            console.log("Estas son las coordenadas " + coord);
+             resolve([childData.longitude,childData.latitude]);
 
         }else{
             console.log("No funca");
         }
     })
-}).then((coord) => {
-    console.log("Estas son las coordenadas " + coord);
-/* = [9.838701,-83.946894];*/
-var map = L.map('map-template').setView(coord, 15);
+})
 
+}
+)
+
+p1.then((value) => {
+	console.log(value+ "1");
+
+	var map = L.map('map-template').setView(value, 15);
+	console.log(value+ "2");
     L.tileLayer('https://a.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
-    var marker = L.marker(coord).bindPopup('Visto por aquí');
+    var marker = L.marker(value).bindPopup('Visto por aquí');
     map.addLayer(marker);
-});
+	
+		console.log(value+ "3");
+
+},reason => {
+console.error(reason);});
