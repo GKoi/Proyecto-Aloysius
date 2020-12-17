2629e2f43f04bc341f1ac5bcb72036dcedf6d972
Gilberth Segura <gilberthsegura@outlook.com>
2020-12-16 18:19:38 -0600

    Actualización de Prueba(Geo)

--- public/js/geo.js
+++ public/js/geo.js
@@ -1,7 +1,26 @@
-var coord = [9.838701,-83.946894];
-var map = L.map('map-template').setView([9.838701,-83.946894], 15);
+var loc = db.ref('Localizaciones');
+var coord = [0,0];
 
-L.tileLayer('https://a.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+loc.on('value', function(snapshot) {
+    snapshot.forEach(function(childSnapshot) {
+      var childData = childSnapshot.val();
+      console.log(childData);
+      if("117140802"==childData.ced){
+        console.log([childData.latitude, childData.longitude]) 
+            coord = [childData.latitude, childData.longitude];
+            console.log("Estas son las coordenadas " + coord);
 
-var marker = L.marker(coord).bindPopup('Visto por aquí');
-map.addLayer(marker);
+        }else{
+            console.log("No funca");
+        }
+    })
+}).then((coord) => {
+    console.log("Estas son las coordenadas " + coord);
+/* = [9.838701,-83.946894];*/
+var map = L.map('map-template').setView(coord, 15);
+
+    L.tileLayer('https://a.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+    var marker = L.marker(coord).bindPopup('Visto por aquí');
+    map.addLayer(marker);
+});
